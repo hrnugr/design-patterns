3f136a3c77d323024f7c1fc90500f8bfbcee9be0
Harun <ugur.harun@yandex.com>
2020-12-23 15:30:32 +0300

    facade

---
+++ src/com/harunugur/patterns/facade/FacadePatternTest.java
@@ -0,0 +1,13 @@
+package com.harunugur.patterns.facade;
+
+public class FacadePatternTest {
+
+    public static void main(String[] args) {
+        String tableName="Employee";
+
+        //generating MySql HTML report and Oracle PDF report using Facade
+        HelperFacade.generateReport(HelperFacade.DBTypes.MYSQL, HelperFacade.ReportTypes.HTML, tableName);
+        HelperFacade.generateReport(HelperFacade.DBTypes.ORACLE, HelperFacade.ReportTypes.PDF, tableName);
+    }
+
+}
---
+++ src/com/harunugur/patterns/facade/MySqlHelper.java
@@ -0,0 +1,16 @@
+package com.harunugur.patterns.facade;
+
+import java.sql.Connection;
+
+public class MySqlHelper {
+
+    public static Connection getMySqlDBConnection(){
+        return null;
+    }
+
+    public void generateMySqlPDFReport(String tableName, Connection con){
+    }
+
+    public void generateMySqlHTMLReport(String tableName, Connection con){
+    }
+}
